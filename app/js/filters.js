d3de918fd87ff4b25315aa5fc6c0663a9d57ada5
Reese Garth <gart6270@mylaurier.ca>
2014-02-08 16:15:59 -0500

    Changed myApp to MixDatUp in js files

--- app/js/filters.js
+++ app/js/filters.js
@@ -2,7 +2,7 @@
 
 /* Filters */
 
-angular.module('myApp.filters', []).
+angular.module('MixDatUp.filters', []).
   filter('interpolate', ['version', function(version) {
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
--- app/js/services.js
+++ app/js/services.js
@@ -5,9 +5,5 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).value('version', '0.1');
-angular.module('myApp.services', ['ngResource']).factory('myApp.services', function($resource) {
-  return $resource('recipes.json',{ }, {
-    getData: {method:'GET', isArray: false}
-  });
+angular.module('MixDatUp.services', []).value('version', '0.1');
 });
