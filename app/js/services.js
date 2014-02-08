6a63a9bb055ef166de303673e78d935189304edb
Reese Garth <gart6270@mylaurier.ca>
2014-02-08 16:09:46 -0500

    Added Controller
    Controller is for Alcoholic Drinks(the recipe json file). Also changed
    myApp to MixDatUp

--- app/js/services.js
+++ app/js/services.js
@@ -5,10 +5,8 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
-angular.module('myApp.serives', ['ngResource'])
-.factory('myApp.serivces', function($resource) {
+angular.module('myApp.services', []).value('version', '0.1');
+angular.module('myApp.services', ['ngResource']).factory('myApp.services', function($resource) {
   return $resource('recipes.json',{ }, {
     getData: {method:'GET', isArray: false}
   });
